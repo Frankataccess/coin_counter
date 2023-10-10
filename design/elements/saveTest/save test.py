bd57fc6b9a0e280bccb55737f82a1a8ab0fd2b0a
Frankataccess <frank.mccann.355@accesscreative.ac.uk>
2023-10-10 09:13:17 +0100

    combined accuracy to main code

--- design/elements/saveTest/save test.py
+++ design/elements/saveTest/save test.py
@@ -20,7 +20,7 @@ else:
 # Gather volunteer information
 volunteer_name = input("Enter volunteer name: ")
 coin_type = input("Enter coin type: ")
-bag_weight = input("Enter bag weight: ")
+volunteer_bag_weight = int(input("Enter bag weight: "))
 bags_correct = 0
 
 #bag counter 
@@ -36,8 +36,58 @@ with open("save_test.txt", 'r') as file:
         # If the loop completes without finding the name
         bags_counted = 1
     
+#accuracy
+# Define correct bag weights
+correct_bag_weights = {
+    "1p": 356,
+    "2p": 356,
+    "5p": 235,
+    "10p": 325,
+    "20p": 250,
+    "50p": 160,
+    "£1": 175,
+    "£2": 120
+}
+
+# Check if the coin type is valid
+if coin_type in correct_bag_weights:
+    correct_bag_weight = correct_bag_weights[coin_type]
+else:
+    print("Please enter a valid coin type")
+    exit()
+
+bags_counted = 0
+
+# Read file and update counts
+with open("save_test.txt", 'r') as file:
+    for line in file:
+        columns = line.strip().split()
+        if columns[0] == volunteer_name:
+            bags_counted = int(columns[5]) + 1
+            bags_correct = int(columns[4])
+            if volunteer_bag_weight == correct_bag_weight:
+                bags_correct += 1
+            break
+    else:
+        # If the name is not found, initialize counts
+        bags_counted = 1
+        if volunteer_bag_weight == correct_bag_weight:
+            bags_correct = 1
+
+# Calculate accuracy
+if bags_counted != 0:
+    accuracy = (bags_correct / bags_counted) * 100
+else:
+    accuracy = 0
+
+# Variable check
+print("bags correct:", bags_correct)
+print("bags counted:", bags_counted)
+print("volunteer bag weight:", volunteer_bag_weight)
+print("correct bag weight", correct_bag_weight)
+print("accuracy:", accuracy)
 # Adding new line
-new_person = [volunteer_name, coin_type, bag_weight, accuracy,bags_correct, bags_counted]
+new_person = [volunteer_name, coin_type, volunteer_bag_weight, accuracy,bags_correct, bags_counted]
 formatted_new_person = "{: <20} {: <20} {: <20} {: <20} {: <20} {: <10}".format(*new_person)
 
 # Append the new row to the file
